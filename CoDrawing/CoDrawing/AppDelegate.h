b9b5d4886bb3adf032661893c0f3fe991923e486
lukaszczarnecki <lczarne@gmail.com>
2013-09-25 22:35:39 +0200

    Xcode project created

---
+++ CoDrawing/CoDrawing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoDrawing
+//
+//  Created by Łukasz Czarnecki on 9/25/13.
+//  Copyright (c) 2013 Łukasz Czarnecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoDrawing/CoDrawing/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoDrawing
+//
+//  Created by Łukasz Czarnecki on 9/25/13.
+//  Copyright (c) 2013 Łukasz Czarnecki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
